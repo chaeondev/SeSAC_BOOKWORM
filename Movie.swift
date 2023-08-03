aad83c5d189386f1c4f9ba3e5c65cd97f383a1c7
chaeondev <ycw0363@gmail.com>
2023-08-04 02:52:17 +0900

    design Main Collection View

--- Movie.swift
+++ Movie.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 struct Movie {
+    
+    static func randomBackgroundColor() -> UIColor {
+            
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        
+        return UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
     let title: String
     let releaseDate: String
     let runtime: Int
     let overview: String
     var rate: Double
     var like: Bool = false
+    var color: UIColor = randomBackgroundColor()
 }
+
